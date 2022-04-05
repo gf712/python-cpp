cfab91241f2c34d8f13ed2848e661fc56b206423
Gil Hoben <gil_f.hoben@hotmail.com>
2022-04-05 08:05:53 +0100

    Added option to run integration tests with debug and trace logging

--- src/testing/main.cpp
+++ src/testing/main.cpp
@@ -2,6 +2,8 @@
 
 #include "gtest/gtest.h"
 
+#include <cxxopts.hpp>
+
 class PythonVMEnvironment : public ::testing::Environment
 {
 	char **m_argv;
@@ -24,6 +26,26 @@ int main(int argc, char **argv)
 	::testing::InitGoogleTest(&argc, argv);
 
 	(void)AddGlobalTestEnvironment(new PythonVMEnvironment(argv));
+
+	cxxopts::Options options("python-testing", "The C++ Python interpreter tests");
+
+	// clang-format off
+	options.add_options()
+		("d,debug", "Enable debug logging", cxxopts::value<bool>()->default_value("false"))
+		("trace", "Enable trace logging", cxxopts::value<bool>()->default_value("false"))
+		("h,help", "Print usage");
+
+	auto result = options.parse(argc, argv);
+
+	if (result.count("help")) {
+		std::cout << options.help() << std::endl;
+		return EXIT_SUCCESS;
+	}
+
+	const bool debug = result["debug"].as<bool>();
+	const bool trace = result["trace"].as<bool>();
+	if (debug) { spdlog::set_level(spdlog::level::debug); }
+	if (trace) { spdlog::set_level(spdlog::level::trace); }
 
 	return RUN_ALL_TESTS();
 }
