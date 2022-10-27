82a7196eb47e7e8e40aea8d34802022576630828
Gil Hoben <gil_f.hoben@hotmail.com>
2022-10-27 08:28:19 +0100

    Added project_options CMake project
    This adds various compiler warnings and makes it easier to enable
    cross-platform build time behaviour (sanitizers, cache, etc)

---
+++ cmake/project_options.cmake
@@ -0,0 +1,10 @@
+# Add project_options v0.25.2
+# https://github.com/aminya/project_options
+# Change the version in the following URL to update the package (watch the releases of the repository for future updates)
+include(FetchContent)
+FetchContent_Declare(_project_options
+    URL https://github.com/aminya/project_options/archive/refs/tags/v0.25.2.zip
+    DOWNLOAD_EXTRACT_TIMESTAMP TRUE
+)
+FetchContent_MakeAvailable(_project_options)
+include(${_project_options_SOURCE_DIR}/Index.cmake)
