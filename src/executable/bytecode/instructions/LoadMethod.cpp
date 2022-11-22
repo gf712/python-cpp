8e1b2ba4e478a06ec359f9ce4b95c8da7750a26e
Gil Hoben <gil_f.hoben@hotmail.com>
2022-11-22 13:56:37 +0000

    Bytecode: Simplified LoadMethod code with monads

--- src/executable/bytecode/instructions/LoadMethod.cpp
+++ src/executable/bytecode/instructions/LoadMethod.cpp
@@ -15,27 +15,16 @@ PyResult<Value> LoadMethod::execute(VirtualMachine &vm, Interpreter &interpreter
 {
 	auto this_value = vm.reg(m_value_source);
 	const auto &method_name = interpreter.execution_frame()->names(m_method_name);
-	spdlog::debug("This object: {}",
-		std::visit(
-			[](const auto &val) {
-				auto obj = PyObject::from(val);
-				ASSERT(obj.is_ok())
-				return obj.unwrap()->to_string();
-			},
-			this_value));
 	auto this_obj_ = PyObject::from(this_value);
 	if (this_obj_.is_err()) { return Err(this_obj_.unwrap_err()); }
 	auto *this_obj = this_obj_.unwrap();
 	auto name = PyString::create(method_name);
-	if (name.is_err()) { return Err(name.unwrap_err()); }
-	auto maybe_method = this_obj->get_method(name.unwrap());
-	if (maybe_method.is_err()) { return maybe_method; }
-	if (auto *method = maybe_method.unwrap(); !method->is_callable()) {
-		return Err(attribute_error("object '{}' is not callable", method->name()));
-	} else {
-		vm.reg(m_destination) = method;
-	}
-	return Ok(Value{ maybe_method.unwrap() });
+	return name
+		.and_then([this_obj](PyString *method_name) { return this_obj->get_method(method_name); })
+		.and_then([&vm, this](PyObject *method_obj) {
+			vm.reg(m_destination) = method_obj;
+			return Ok(method_obj);
+		});
 }
 
 std::vector<uint8_t> LoadMethod::serialize() const
