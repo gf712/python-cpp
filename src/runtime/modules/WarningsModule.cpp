bb984a3c167f27b4370de2dcc38ae21da740bb5a
Gil Hoben <gil_f.hoben@hotmail.com>
2023-03-10 16:57:00 +0000

    Runtime: Added _warnings.warn stub

--- src/runtime/modules/WarningsModule.cpp
+++ src/runtime/modules/WarningsModule.cpp
@@ -1,5 +1,10 @@
 #include "Modules.hpp"
 #include "runtime/PyDict.hpp"
+#include "runtime/PyFunction.hpp"
+#include "runtime/PyNone.hpp"
+#include "runtime/PyString.hpp"
+#include "runtime/PyType.hpp"
+#include "runtime/types/builtin.hpp"
 
 namespace py {
 PyModule *warnings_module()
@@ -8,6 +13,12 @@ PyModule *warnings_module()
 		PyString::create("_warnings").unwrap(),
 		PyString::create("").unwrap())
 								  .unwrap();
+
+	s_warnings_module->add_symbol(PyString::create("warn").unwrap(),
+		PyNativeFunction::create("warn", [](PyTuple *, PyDict *) {
+			// TODO: Implement _warnings.warn
+			return Ok(py_none());
+		}).unwrap());
 
 	return s_warnings_module;
 }
