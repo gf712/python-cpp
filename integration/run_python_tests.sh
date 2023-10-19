2ba7e71e916d5a5293d33f3411ed619e8f208742
gf712 <gil_f.hoben@hotmail.com>
2023-10-19 20:13:18 +0100

    Tests: simplified bash scripts

--- integration/run_python_tests.sh
+++ integration/run_python_tests.sh
@@ -5,14 +5,15 @@
 SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
 PYTHON_EXECUTABLE=$1
+GC_FREQUENCY=100000
 
 # start by calling the tests that we need to work in order to trust the result of the other python tests
-if timeout 10s $PYTHON_EXECUTABLE $SCRIPT_DIR/tests/lemmas/assert_false.py --gc-frequency 1; then
+if timeout 10s $PYTHON_EXECUTABLE $SCRIPT_DIR/tests/lemmas/assert_false.py --gc-frequency $GC_FREQUENCY; then
     echo "assert_false.py failed"
     exit 1
 fi
 
-if !(timeout 10s $PYTHON_EXECUTABLE $SCRIPT_DIR/tests/lemmas/assert_true.py --gc-frequency 1); then
+if !(timeout 10s $PYTHON_EXECUTABLE $SCRIPT_DIR/tests/lemmas/assert_true.py --gc-frequency $GC_FREQUENCY); then
     echo "assert_true.py failed"
     exit 1
 fi
@@ -20,7 +21,7 @@ fi
 exit_code=0
 
 for file in $(find $SCRIPT_DIR/tests/ -maxdepth 1 -type f -name "*.py"); do
-    result=$(timeout 10s $PYTHON_EXECUTABLE $file --gc-frequency 1)
+    result=$(timeout 10s $PYTHON_EXECUTABLE $file --gc-frequency $GC_FREQUENCY)
     retval=$?
     if [ $retval -eq 0 ]; then
         echo $file "... PASSED!"
