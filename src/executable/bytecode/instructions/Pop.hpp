931926ee1a13628cb45543438fa0bba70125142c
gf712 <gil_f.hoben@hotmail.com>
2024-04-17 07:53:50 +0100

    Bytecode: Added option to Pop to discard value
    Sometimes we don't need to pop a value to register and can just discard
    it.

--- src/executable/bytecode/instructions/Pop.hpp
+++ src/executable/bytecode/instructions/Pop.hpp
@@ -4,9 +4,21 @@
 
 class Pop final : public Instruction
 {
+	bool m_discard{ true };
+	Register m_dst;
+
   public:
 	Pop() {}
-	std::string to_string() const final { return "POP"; }
+	Pop(Register dst) : Pop(dst, false) {}
+	Pop(Register dst, bool discard) : m_discard(discard), m_dst(dst) {}
+	std::string to_string() const final
+	{
+		if (m_discard) {
+			return "POP";
+		} else {
+			return fmt::format("POP             r{}", m_dst);
+		}
+	}
 	py::PyResult<py::Value> execute(VirtualMachine &vm, Interpreter &interpreter) const final;
 
 	void relocate(size_t) final {}
