ea8bfa919526f82afc8f45aa36658dd0cf800339
gf712 <gil_f.hoben@hotmail.com>
2024-04-17 09:35:47 +0100

    Utilities: Use MLIR backend in freeze utility.
    Also converted importlib to frozen bytecode using the new backend.

--- src/utilities/freeze.cpp
+++ src/utilities/freeze.cpp
@@ -3,10 +3,18 @@
 #include "executable/bytecode/Bytecode.hpp"
 #include "executable/bytecode/BytecodeProgram.hpp"
 #include "executable/bytecode/codegen/BytecodeGenerator.hpp"
+#include "executable/mlir/Conversion/Passes.hpp"
+#include "executable/mlir/Conversion/PythonToPythonBytecode/PythonToPythonBytecode.hpp"
+#include "executable/mlir/Dialect/Python/MLIRGenerator.hpp"
+#include "executable/mlir/Target/PythonBytecode/PythonBytecodeEmitter.hpp"
 #include "interpreter/Interpreter.hpp"
 #include "parser/Parser.hpp"
 #include "runtime/types/builtin.hpp"
 #include "vm/VM.hpp"
+
+#include "mlir/IR/BuiltinOps.h"
+#include "mlir/Pass/Pass.h"
+#include "mlir/Pass/PassManager.h"
 
 #include <cxxopts.hpp>
 
@@ -14,6 +22,32 @@
 #include <fstream>
 
 using namespace py;
+
+namespace {
+std::shared_ptr<BytecodeProgram> compile(const std::string &filename, std::vector<std::string> argv)
+{
+	auto lexer = Lexer::create(std::filesystem::absolute(filename));
+	parser::Parser p{ lexer };
+	p.parse();
+
+	auto ctx = codegen::Context::create();
+	if (!codegen::MLIRGenerator::compile(p.module(), std::move(argv), ctx)) {
+		std::cerr << "Failed to compile Python script\n";
+		return nullptr;
+	}
+
+	mlir::py::registerConversionPasses();
+
+	mlir::PassManager pm{ &ctx.ctx() };
+	pm.addPass(mlir::py::createPythonToPythonBytecodePass());
+	if (pm.run(ctx.module()).failed()) {
+		std::cerr << "Python bytecode MLIR lowering failed\n";
+		ctx.module().dump();
+		return nullptr;
+	}
+
+	return codegen::translateToPythonBytecode(ctx.module());
+}
 
 int freeze(size_t argc, char **argv, const std::string &output)
 {
@@ -24,13 +58,8 @@ int freeze(size_t argc, char **argv, const std::string &output)
 	while (arg_idx < argc) { argv_vector.emplace_back(argv[arg_idx++]); }
 
 	[[maybe_unused]] auto &vm = VirtualMachine::the();
-	initialize_types();
-	auto lexer = Lexer::create(std::filesystem::absolute(filename));
-	parser::Parser p{ lexer };
-	p.parse();
-	auto bytecode = codegen::BytecodeGenerator::compile(
-		p.module(), argv_vector, compiler::OptimizationLevel::None);
 
+	auto bytecode = compile(filename, std::move(argv_vector));
 	std::cout << bytecode->to_string() << "-----------------------------\n\n";
 
 	const auto bytes = bytecode->serialize();
@@ -51,6 +80,7 @@ int freeze(size_t argc, char **argv, const std::string &output)
 	out.close();
 	return EXIT_SUCCESS;
 }
+}// namespace
 
 int main(int argc, char **argv)
 {
