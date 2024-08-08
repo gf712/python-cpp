f81253d31d29d8b30125557a9a04f21d042b1f2d
gf712 <gil_f.hoben@hotmail.com>
2024-08-08 17:05:21 +0100

    Runtime: Implemented a couple more bytearray methods

--- src/runtime/PyByteArray.cpp
+++ src/runtime/PyByteArray.cpp
@@ -3,13 +3,22 @@
 #include "PyBytes.hpp"
 #include "StopIteration.hpp"
 #include "runtime/IndexError.hpp"
+#include "runtime/PyDict.hpp"
 #include "runtime/PyInteger.hpp"
+#include "runtime/PyObject.hpp"
+#include "runtime/PySlice.hpp"
 #include "runtime/TypeError.hpp"
+#include "runtime/Value.hpp"
 #include "runtime/ValueError.hpp"
 #include "types/api.hpp"
 #include "types/builtin.hpp"
 #include "utilities.hpp"
 #include "vm/VM.hpp"
+
+#include <algorithm>
+#include <cstddef>
+#include <iterator>
+#include <numeric>
 
 namespace py {
 PyByteArray::PyByteArray(PyType *type) : PyBaseObject(type) {}
@@ -97,11 +106,11 @@ PyResult<int32_t> PyByteArray::__init__(PyTuple *args, PyDict *kwargs)
 std::string PyByteArray::to_string() const
 {
 	std::ostringstream os;
-	os << "bytearray(b'";
-	os << m_value.to_string();
-	os << "')";
+	os << "bytearray(" << m_value.to_string() << ")";
 	return os.str();
 }
+
+PyResult<size_t> PyByteArray::__len__() const { return Ok(m_value.b.size()); }
 
 PyResult<PyObject *> PyByteArray::__iter__() const
 {
@@ -142,6 +151,231 @@ PyResult<std::monostate> PyByteArray::__setitem__(int64_t index, PyObject *value
 	return Ok(std::monostate{});
 }
 
+PyResult<PyObject *> PyByteArray::__getitem__(PyObject *index)
+{
+	if (index->type()->issubclass(types::integer())) {
+		const auto i = static_cast<const PyInteger &>(*index).as_i64();
+		return __getitem__(i);
+	} else if (index->type()->issubclass(types::slice())) {
+		auto slice = static_cast<PySlice *>(index);
+		auto indices_ = slice->unpack();
+		if (indices_.is_err()) return Err(indices_.unwrap_err());
+		const auto [start_, end_, step] = indices_.unwrap();
+
+		const auto [start, end, slice_length] =
+			PySlice::adjust_indices(start_, end_, step, m_value.b.size());
+
+		if (slice_length == 0) { return PyByteArray::create(); }
+		if (start == 0 && end == static_cast<int64_t>(m_value.b.size()) && step == 1) {
+			return PyByteArray::create(m_value);
+		}
+
+		Bytes bytes;
+		bytes.b.reserve(slice_length);
+		for (int64_t idx = start, i = 0; i < slice_length; idx += step, ++i) {
+			bytes.b.push_back(m_value.b[idx]);
+		}
+
+		return PyByteArray::create(bytes);
+	} else {
+		return Err(type_error(
+			"bytearray indices must be integers or slices, not {}", index->type()->name()));
+	}
+}
+
+PyResult<PyObject *> PyByteArray::find(PyTuple *args, PyDict *kwargs) const
+{
+	ASSERT(args && args->size() <= 3 && args->size() > 0)
+	ASSERT(!kwargs)
+
+	auto pattern_ = PyObject::from(args->elements()[0]);
+	if (pattern_.is_err()) return pattern_;
+	if (!pattern_.unwrap()->type()->issubclass(types::integer())) { TODO(); }
+	const auto &pattern_int = static_cast<const PyInteger &>(*pattern_.unwrap());
+	if (pattern_int.as_big_int() < 0 && pattern_int.as_big_int() > 255) { TODO(); }
+	PyInteger *start = nullptr;
+	PyInteger *end = nullptr;
+	int64_t result = -1;
+
+	if (args->size() >= 2) {
+		auto start_ = PyObject::from(args->elements()[1]);
+		if (start_.is_err()) return start_;
+		start = as<PyInteger>(start_.unwrap());
+		// TODO: raise exception when start in not a number
+		ASSERT(start)
+	}
+	if (args->size() == 3) {
+		auto end_ = PyObject::from(args->elements()[2]);
+		if (end_.is_err()) return end_;
+		end = as<PyInteger>(end_.unwrap());
+		// TODO: raise exception when end in not a number
+		ASSERT(end)
+	}
+
+	auto get_position_from_slice = [this](int64_t pos) -> PyResult<size_t> {
+		if (pos < 0) {
+			pos += m_value.b.size();
+			// TODO: handle case where the negative start index is less than size of string
+			if (pos < 0) { return Err(index_error("bytearray index out of range")); }
+		}
+		return Ok(pos);
+	};
+
+	auto find = [value = static_cast<std::byte>(pattern_int.as_size_t())](
+					std::span<const std::byte> bytes, size_t start) -> int64_t {
+		auto it = std::ranges::find(bytes, value);
+		if (it == bytes.end()) { return -1; }
+		return start + std::distance(bytes.begin(), it);
+	};
+
+	if (!start && !end) {
+		result = find(m_value.b, 0);
+	} else if (!end) {
+		auto start_ =
+			std::visit(overloaded{
+						   [get_position_from_slice](const auto &val) -> PyResult<size_t> {
+							   return get_position_from_slice(static_cast<int64_t>(val));
+						   },
+						   [get_position_from_slice](const mpz_class &val) -> PyResult<size_t> {
+							   ASSERT(val.fits_slong_p());
+							   return get_position_from_slice(val.get_si());
+						   },
+					   },
+				start->value().value);
+		if (start_.is_err()) { return Err(start_.unwrap_err()); }
+		result = find(
+			std::span{ m_value.b.begin() + start_.unwrap(), m_value.b.end() }, start_.unwrap());
+	} else {
+		auto start_ =
+			std::visit(overloaded{
+						   [get_position_from_slice](const auto &val) -> PyResult<size_t> {
+							   return get_position_from_slice(static_cast<int64_t>(val));
+						   },
+						   [get_position_from_slice](const mpz_class &val) -> PyResult<size_t> {
+							   ASSERT(val.fits_slong_p());
+							   return get_position_from_slice(val.get_si());
+						   },
+					   },
+				start->value().value);
+		if (start_.is_err()) { return Err(start_.unwrap_err()); }
+		auto end_ =
+			std::visit(overloaded{
+						   [get_position_from_slice](const auto &val) -> PyResult<size_t> {
+							   return get_position_from_slice(static_cast<int64_t>(val));
+						   },
+						   [get_position_from_slice](const mpz_class &val) -> PyResult<size_t> {
+							   ASSERT(val.fits_slong_p());
+							   return get_position_from_slice(val.get_si());
+						   },
+					   },
+				end->value().value);
+		if (end_.is_err()) { return Err(end_.unwrap_err()); }
+		result = find(
+			std::span{ m_value.b.begin() + start_.unwrap(), m_value.b.begin() + end_.unwrap() },
+			start_.unwrap());
+	}
+
+	return PyInteger::create(result);
+}
+
+PyResult<PyObject *> PyByteArray::maketrans(PyObject *from, PyObject *to)
+{
+	Bytes from_bytes;
+	if (from->type()->issubclass(types::bytes())) {
+		from_bytes = static_cast<const PyBytes &>(*from).value();
+	} else if (from->type()->issubclass(types::bytearray())) {
+		from_bytes = static_cast<const PyByteArray &>(*from).value();
+	} else {
+		return Err(type_error("a bytes-like object is required, not '{}'", from->type()->name()));
+	}
+
+	Bytes to_bytes;
+	if (to->type()->issubclass(types::bytes())) {
+		to_bytes = static_cast<const PyBytes &>(*to).value();
+	} else if (to->type()->issubclass(types::bytearray())) {
+		to_bytes = static_cast<const PyByteArray &>(*to).value();
+	} else {
+		return Err(type_error("a bytes-like object is required, not '{}'", from->type()->name()));
+	}
+
+	if (from_bytes.b.size() != to_bytes.b.size()) {
+		return Err(value_error("maketrans arguments must have same length"));
+	}
+
+	Bytes result;
+	result.b.reserve(256);
+	for (size_t i = 0; i < 256; ++i) { result.b.push_back(static_cast<std::byte>(i)); }
+	for (size_t i = 0; i < 256; ++i) {
+		const auto from_byte = from_bytes.b[i];
+		const auto to_byte = to_bytes.b[i];
+		result.b[static_cast<size_t>(from_byte)] = to_byte;
+	}
+
+	return PyByteArray::create(result);
+}
+
+PyResult<PyObject *> PyByteArray::translate(PyTuple *args, PyDict *kwargs) const
+{
+	Bytes table;
+	Bytes to_delete;
+	if (!args || args->size() == 0) {
+		return Err(type_error(
+			"translate() takes at least 1 positional argument ({} given)", args->size()));
+	}
+	if (auto argcount = (args->size() + (kwargs ? kwargs->size() : 0)); argcount > 2) {
+		return Err(type_error("translate() takes at most 2 arguments ({} given)", argcount));
+	}
+	auto el0 = PyObject::from(args->elements()[0]).unwrap();
+	if (el0->type()->issubclass(types::bytes())) {
+		table = static_cast<const PyBytes &>(*el0).value();
+	} else if (el0->type()->issubclass(types::bytearray())) {
+		table = static_cast<const PyByteArray &>(*el0).value();
+	} else {
+		return Err(type_error("a bytes-like object is required, not '{}'", el0->type()->name()));
+	}
+
+	if (args->size() == 2) {
+		auto *el1 = PyObject::from(args->elements()[1]).unwrap();
+		if (el1->type()->issubclass(types::bytes())) {
+			to_delete = static_cast<const PyBytes &>(*el1).value();
+		} else if (el1->type()->issubclass(types::bytearray())) {
+			to_delete = static_cast<const PyByteArray &>(*el1).value();
+		} else {
+			return Err(
+				type_error("a bytes-like object is required, not '{}'", el1->type()->name()));
+		}
+	} else if (kwargs) {
+		if (auto it = kwargs->map().find(String{ "delete" }); it != kwargs->map().end()) {
+			auto *el1 = PyObject::from(it->second).unwrap();
+			if (el1->type()->issubclass(types::bytes())) {
+				to_delete = static_cast<const PyBytes &>(*el1).value();
+			} else if (el1->type()->issubclass(types::bytearray())) {
+				to_delete = static_cast<const PyByteArray &>(*el1).value();
+			} else {
+				return Err(
+					type_error("a bytes-like object is required, not '{}'", el1->type()->name()));
+			}
+		}
+	}
+
+	if (!table.b.empty() && table.b.size() != 256) {
+		return Err(value_error("translation table must be 256 characters long"));
+	}
+
+	Bytes result;
+	std::ranges::remove_copy_if(
+		m_value.b, std::back_inserter(result.b), [&table](const auto &el) -> bool {
+			return std::ranges::find(table.b, el) != table.b.end();
+		});
+
+	if (!table.b.empty()) {
+		std::ranges::transform(result.b, result.b.begin(), [&table](const auto el) {
+			return table.b[static_cast<size_t>(el)];
+		});
+	}
+
+	return PyByteArray::create(result);
+}
 
 namespace {
 
@@ -149,7 +383,11 @@ namespace {
 
 	std::unique_ptr<TypePrototype> register_bytearray()
 	{
-		return std::move(klass<PyByteArray>("bytearray").type);
+		return std::move(klass<PyByteArray>("bytearray")
+							 .def("find", &PyByteArray::find)
+							 .def("translate", &PyByteArray::translate)
+							 .staticmethod("maketrans", &PyByteArray::maketrans)
+							 .type);
 	}
 }// namespace
 
--- src/runtime/PyByteArray.hpp
+++ src/runtime/PyByteArray.hpp
@@ -1,6 +1,8 @@
 #pragma once
 
 #include "PyObject.hpp"
+#include "runtime/PyDict.hpp"
+#include "runtime/PyTuple.hpp"
 
 namespace py {
 
@@ -24,11 +26,18 @@ class PyByteArray : public PyBaseObject
 
 	PyResult<PyObject *> __iter__() const;
 	PyResult<PyObject *> __repr__() const;
+	PyResult<size_t> __len__() const;
 
 	PyResult<PyObject *> __getitem__(int64_t index);
 	PyResult<std::monostate> __setitem__(int64_t index, PyObject *value);
 
+	PyResult<PyObject *> __getitem__(PyObject *index);
+
 	const Bytes &value() const { return m_value; }
+
+	PyResult<PyObject *> find(PyTuple *args, PyDict *kwargs) const;
+	static PyResult<PyObject *> maketrans(PyObject *from, PyObject *to);
+	PyResult<PyObject *> translate(PyTuple *args, PyDict *kwargs) const;
 
 	static std::function<std::unique_ptr<TypePrototype>()> type_factory();
 	PyType *static_type() const override;
