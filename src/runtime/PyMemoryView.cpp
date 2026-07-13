3f7ca4cbb286c3b065d2c34a624cb0f1c72a9f94
gf712 <gil_f.hoben@hotmail.com>
2026-07-13 15:58:21 +0100

    runtime: start respecting buffer flags

--- src/runtime/PyMemoryView.cpp
+++ src/runtime/PyMemoryView.cpp
@@ -343,9 +343,13 @@ PyResult<PyObject *> PyMemoryView::tolist()
 
 PyResult<PyObject *> PyMemoryView::__repr__() const { return PyString::create(to_string()); }
 
-PyResult<std::monostate> PyMemoryView::__getbuffer__(PyBuffer &view, int /*flags*/)
+PyResult<std::monostate> PyMemoryView::__getbuffer__(PyBuffer &view, int flags)
 {
-	// TODO: validate flags
+	// PyBUF_WRITABLE == 1
+	if ((flags & 1) && m_view.readonly) {
+		return Err(type_error("cannot use a read-only memoryview as a read-write buffer"));
+	}
+	// TODO: validate the remaining flags
 	view = PyBuffer{
 		.buf = m_view.buf->view(),
 		.obj = this,
